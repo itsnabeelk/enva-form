689a33b9f79c4fcac191f118ddaa74c3dec167e5
nabeelknk <nabeel@veuz.in>
2024-04-27 02:21:22 +0300

    add

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ gsap.from(".col-md-6 .form-group", {
   duration: 1,
   scrollTrigger: {
     trigger: ".col-md-6 .form-group",
-    toggleActions: "restart restart restart reverse",
+    toggleActions: "play restart restart reverse",
   },
   y: -300,
   stagger: 0.3,
@@ -14,7 +14,7 @@ gsap.from(".logo-container .image-container img", {
     duration: 1,
     scrollTrigger: {
       trigger: ".logo-container .image-container img",
-      toggleActions: "restart restart restart reverse",
+      toggleActions: "reverse restart restart reverse",
     },
     y: -300,
     stagger: 0.3,
