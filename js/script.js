e1d21b223619b1c46ab564123fd45e30c1d3424e
nabeelknk <nabeel@veuz.in>
2024-04-27 01:42:09 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+gsap.registerPlugin(ScrollTrigger);
+// anim-text
+gsap.from(".col-md-6 .form-group", {
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".col-md-6 .form-group",
+    toggleActions: "restart restart restart reverse",
+  },
+  y: -300,
+  stagger: 0.3,
+});
+gsap.from(".logo-container .image-container img", {
+    duration: 1,
+    scrollTrigger: {
+      trigger: ".logo-container .image-container img",
+      toggleActions: "restart restart restart reverse",
+    },
+    y: -300,
+    stagger: 0.3,
+  });
+// text-animation-end
+
+
