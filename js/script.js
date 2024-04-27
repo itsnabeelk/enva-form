24e2b65a9b88541197f39822aa6649e22f91495e
nabeelknk <nabeel@veuz.in>
2024-04-27 13:47:26 +0300

    add

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ gsap.from(".col-md-6 .form-group", {
   duration: 1,
   scrollTrigger: {
     trigger: ".col-md-6 .form-group",
-    toggleActions: "play restart restart reverse",
+    toggleActions: "play play play play",
   },
   y: -300,
   stagger: 0.3,
@@ -14,7 +14,7 @@ gsap.from(".logo-container .image-container img", {
     duration: 1,
     scrollTrigger: {
       trigger: ".logo-container .image-container img",
-      toggleActions: "reverse restart restart reverse",
+      toggleActions: "play play play play",
     },
     y: -300,
     stagger: 0.3,
@@ -22,3 +22,13 @@ gsap.from(".logo-container .image-container img", {
 // text-animation-end
 
 
+// Get the select element
+const dropdown = document.getElementById('dropdown');
+
+// Set the initial focus color to blue
+dropdown.style.color = '#ccc';
+
+// Add an event listener to change the color to black when an option is selected
+dropdown.addEventListener('change', function() {
+  this.style.color = '#000';
+});
